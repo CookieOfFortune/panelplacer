9bb00e26568269d1ac2b71eea1b35ecd45085ec9
Peng Chen <cookieoffortune@gmail.com>
2021-08-15 01:48:12 -0700

    Fix sizing calculation and resizing issues.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,35 +3,64 @@ import logo from './panel.jpg';
 import './App.css';
 import Draggable from './components/Draggable';
 
-type Props = {
+const BG_WIDTH_INCH = 102.7;
+const PANEL_HEIGHT_INCH = 36;
 
-};
+interface Size {
+  width: number | undefined;
+  height: number | undefined;
+}
 
-type State = {
+function App() {
+  const size: Size = useWindowSize();
 
-};
-
-class App extends React.Component<Props, State> {
-  state: State = {};
-
-  render() {
-    return (
-      <div className="App">
-        <Draggable initialPosition={{x: 100, y: 100}}>
-          <img src={logo} className="App-logo" alt="logo" />
-        </Draggable>
-        <Draggable initialPosition={{x: 200, y: 200}}>
-          <img src={logo} className="App-logo" alt="logo" />
-        </Draggable>
-        <Draggable initialPosition={{x: 300, y: 300}}>
-          <img src={logo} className="App-logo" alt="logo" />
-        </Draggable>
-        <Draggable initialPosition={{x: 400, y: 400}}>
-          <img src={logo} className="App-logo" alt="logo" />
-        </Draggable>
-      </div>
-    );
+  let height = 0;
+  if (size.width != null) {
+    height = (size.width / BG_WIDTH_INCH) * PANEL_HEIGHT_INCH;
   }
+
+  let widthOffset = 100;
+  if (size.width != null) {
+    widthOffset = size.width / 4;
+  }
+
+  const panels = [1, 2, 3, 4].map(i => {
+    return (
+      <Draggable key={i} initialPosition={{ x: widthOffset * i, y: height }}>
+        <img src={logo} alt={"panel" + i} height={height + "px"} />
+      </Draggable>);
+  });
+
+  return (
+    <div className="App">
+      {panels}
+    </div>
+  );
+}
+
+function useWindowSize(): Size {
+
+  const [windowSize, setWindowSize] = React.useState<Size>({
+    width: undefined,
+    height: undefined,
+  });
+
+  React.useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowSize;
 }
 
 export default App;
