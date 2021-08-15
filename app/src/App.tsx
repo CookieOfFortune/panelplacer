69d1884dacbeeed03b1f5299604cc698bdf5fad6
Peng Chen <cookieoffortune@gmail.com>
2021-08-15 02:13:20 -0700

    Fix mobile bugs.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -55,8 +55,8 @@ function useWindowSize(): Size {
   React.useEffect(() => {
     function handleResize() {
       setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
+        width: document.documentElement.clientWidth,
+        height: document.documentElement.clientHeight,
       });
     }
 
